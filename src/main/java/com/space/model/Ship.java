a4b24a3952fc0610838c9ef60201cd8fe1febd4c
SergioViacheslaev <internetwalker@yandex.ru>
2019-09-10 14:27:07 +0300

    Add model Ship

--- src/main/java/com/space/model/Ship.java
+++ src/main/java/com/space/model/Ship.java
@@ -1,19 +1,41 @@
 package com.space.model;
 
 
-import javax.persistence.Entity;
+import javax.persistence.*;
 import java.util.Date;
 
-
+@Entity
+@Table(name = "ship")
 public class Ship {
+
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(name = "name")
     private String name;
+
+    @Column(name = "planet")
     private String planet;
+
+    @Column(name = "shipType")
+    @Enumerated(EnumType.STRING)
     private ShipType shipType;
+
+    @Column(name = "prodDate")
     private Date prodDate;
+
+    @Column(name = "isUsed")
     private Boolean isUsed;
+
+    @Column(name = "speed")
     private Double speed;
+
+    @Column(name = "crewSize")
     private Integer crewSize;
+
+    @Column(name = "rating")
     private Double rating;
 
     public Long getId() {
